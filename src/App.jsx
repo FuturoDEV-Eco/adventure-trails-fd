1a37bab65852e2440fcf1718320298d6f800b0d3
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2024-04-12 08:22:20 -0300

    codigos semana 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import CardTrilha from "./components/CardTrilha"
-import "./App.css"
+import CardTrilha from "./components/CardTrilha";
+import "./App.css";
+import useFetch from "./hooks/useFetch";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
 
 function App() {
+  const [dados, isLoading] = useFetch("../data/dados.json");
+  const [trilhas, setTrilhas] = useState([]);
 
-  const listaTrilhas = [
-    {
-      nomeTrilha: "Trilha da Costa da Lagoa",
-      cidade: "Florianópolis",
-      estado: "SC",
-      duracao: 120,
-      trajeto: 4,
-      dificuldade: "iniciante",
-      tipo: "caminhada / trekking",
-      nomeUsuario: "Guilherme André",
-      urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      // urlImagem: "./assets/imagem-trilha-exemplo.jpg"
+  useEffect(() => {
+    if (!!dados) {
+      setTrilhas(dados.trilhas);
     }
-  ]
+  }, [dados]);
 
   return (
-    <div className="container">
-      <h1 className="titulo">Explore trilhas incríveis</h1>
-      {
-        listaTrilhas.map((trilha, index) => (
-          <CardTrilha dadosTrilha={trilha} key={index} />
-        ))
-      }
-    </div>
-  )
+    <>
+      <Header />
+      <div className="container">
+        <h1 className="titulo">Explore trilhas incríveis</h1>
+        {Array.isArray(trilhas) &&
+          trilhas.map((trilha, index) => (
+            <CardTrilha dadosTrilha={trilha} key={index} />
+          ))}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
