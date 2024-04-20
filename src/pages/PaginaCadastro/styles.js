27c4d137668f2b848724d337b6644139d1f7687b
danielly-kammers <daniellyckammers@thomsonreuters.com>
2024-04-19 21:16:18 -0300

    feat: 🎸 criar cadastro de trilhas

---
+++ src/pages/PaginaCadastro/styles.js
@@ -0,0 +1,16 @@
+import { styled } from "@mui/system";
+
+export const PageTitle = styled("h1")({
+ marginBottom: "2rem"
+});
+
+export const FormComponent = styled("form")({
+ display: "flex",
+ flexDirection: "column",
+ gap: "1rem"
+});
+
+export const InputWrapper = styled("div")({
+ display: "flex",
+ flexDirection: "column"
+});
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,29 +1,29 @@
-import {createBrowserRouter} from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom";
 
-import App from "../App"
-import PaginaInicial from "../pages/PaginaInicial"
-import PaginaCadastroTrilha from "../pages/PaginaCadastroTrilha"
-import PaginaListaTrilhas from "../pages/PaginaListaTrilhas"
+import App from "../App";
+import PaginaInicial from "../pages/PaginaInicial";
+import PaginaListaTrilhas from "../pages/PaginaListaTrilhas";
+import PaginaCadastroTrilha from "../pages/PaginaCadastro";
 
 const routes = createBrowserRouter([
-  {
+ {
+  path: "/",
+  element: <App />,
+  children: [
+   {
     path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <PaginaInicial />
-      },
-      {
-        path: "/cadastro-trilha",
-        element: <PaginaCadastroTrilha />
-      },
-      {
-        path: "/lista-trilhas",
-        element: <PaginaListaTrilhas />
-      },
-    ]
-  }
-])
+    element: <PaginaInicial />
+   },
+   {
+    path: "/cadastro-trilha",
+    element: <PaginaCadastroTrilha />
+   },
+   {
+    path: "/lista-trilhas",
+    element: <PaginaListaTrilhas />
+   }
+  ]
+ }
+]);
 
-export default routes
+export default routes;
